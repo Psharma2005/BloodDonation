f2a30e472850a7cb0ace8d4f036af4253ad64d29
Pratyush Sharma <pratyush.sharma@sjsu.edu>
2019-11-10 13:12:14 -0800

    Donor Request API Added

---
+++ src/main/java/com/alokjgoyal/DonorServer/service/RequestService.java
@@ -0,0 +1,49 @@
+package com.alokjgoyal.DonorServer.service;
+
+
+import com.alokjgoyal.DonorServer.model.BloodRequest;
+import com.alokjgoyal.DonorServer.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    RequestRepository requestRepository;
+
+
+    public String addDonorRequest(@RequestBody BloodRequest request) {
+
+        UUID id = UUID.randomUUID();
+        request.setId(id);
+        Format formatter = new SimpleDateFormat("EEEE, dd-MM-yyyy, hh:mm:ss.SSS a");
+        String today = formatter.format(new Date());
+
+        request.setRequested_date(today);
+        requestRepository.save(request);
+        return "Donor Request saved with ID: " + id;
+    }
+
+
+    public List<BloodRequest> getAllRequests() {
+        return requestRepository.findAll();
+    }
+
+    public Optional<BloodRequest> getDonorRequestById(UUID id) {
+        return requestRepository.findById(id);
+    }
+
+    public BloodRequest updateRequest(BloodRequest request) {
+        requestRepository.save(request);
+        return request;
+    }
+}
